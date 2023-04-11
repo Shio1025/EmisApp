9e2703c6d138db2f0ea04fd9b97186b23ff3a6f7
Shio Birbichadze <sbirbichadze@bog.ge>
2023-04-11 19:20:28 +0400

    add some basic modules

---
+++ Emis/BrandBook/BrandBook/BrandBook.h
@@ -0,0 +1,18 @@
+//
+//  BrandBook.h
+//  BrandBook
+//
+//  Created by Shio Birbichadze on 11.04.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BrandBook.
+FOUNDATION_EXPORT double BrandBookVersionNumber;
+
+//! Project version string for BrandBook.
+FOUNDATION_EXPORT const unsigned char BrandBookVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BrandBook/PublicHeader.h>
+
+
---
+++ Emis/SSO/SSO/SSO.h
@@ -0,0 +1,18 @@
+//
+//  SSO.h
+//  SSO
+//
+//  Created by Shio Birbichadze on 11.04.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SSO.
+FOUNDATION_EXPORT double SSOVersionNumber;
+
+//! Project version string for SSO.
+FOUNDATION_EXPORT const unsigned char SSOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSO/PublicHeader.h>
+
+
