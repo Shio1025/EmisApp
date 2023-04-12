f3ccdbc90d565ddc78fcd7089f040e45e87a0ada
Shio Birbichadze <sbirbichadze@bog.ge>
2023-04-12 21:12:17 +0400

    - add lottie support to BrandBook module - add some assets - change deployment targets

---
+++ Emis/BrandBook/BrandBook/Lottie/LottieHandler.swift
@@ -0,0 +1,15 @@
+//
+//  LottieHandler.swift
+//  BrandBook
+//
+//  Created by Shio Birbichadze on 12.04.23.
+//
+
+
+extension BrandBookManager {
+    public struct Lottie {
+        public static let student_1 = "student_1"
+        public static let student_2 = "student_2"
+        public static let student_3 = "student_3"
+    }
+}
--- Emis/Emis/Scenes/ViewController.swift
+++ Emis/Emis/Scenes/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 import BrandBook
+import Lottie
 
 class ViewController: UIViewController {
-
+    
+    let animationView: AnimationView = {
+        let animation = AnimationView()
+        animation.translatesAutoresizingMaskIntoConstraints = false
+        animation.backgroundBehavior = .pauseAndRestore
+        animation.contentMode = .scaleAspectFit
+        animation.loopMode = .playOnce
+        animation.height(equalTo: 200)
+        animation.width(equalTo: 200)
+        return animation
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = BrandBookManager.Color.Theme.Background.layer.uiColor
+        self.view.backgroundColor = BrandBookManager.Color.Theme.Component.solid500.uiColor
+        animationView.animation = .named(BrandBookManager.Lottie.student_2,bundle: Bundle(identifier: "Shio.BrandBook")!)
+        self.view.addSubview(animationView)
+        animationView.centerVertically(to: self.view)
+        animationView.centerHorizontally(to: self.view)
     }
 }
 
