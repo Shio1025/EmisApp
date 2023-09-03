ff420d78459fb1402e99376bf69fa089530258ec
Shio Birbichadze <sbirbichadze@bog.ge>
2023-09-03 18:34:02 +0400

    Add some UI logic to registered subjects page

--- Emis/Emis/Scenes/Registered Subjects/RegisteredSubjectsViewModel.swift
+++ Emis/Emis/Scenes/Registered Subjects/RegisteredSubjectsViewModel.swift
@@ -18,6 +18,10 @@ final class RegisteredSubjectsViewModel {
     @Published private var isLoading: Bool = true
     @Published private var statusBanner: StatusBannerViewModel?
     @Injected private var SSO: SSOManager
+    @Injected private var getRegisteredSubjectsUseCase: GetRegisteredSubjectsUseCase
+    @Injected private var deleteRegisteredSubjectUseCase: DeleteRegisteredSubjectUseCase
+    
+    private var registeredSubjects: [RegisteredSubject]?
     
     var listCellModels: AnyPublisher<[any CellModel], Never> {
         $listCells.eraseToAnyPublisher()
@@ -55,11 +59,30 @@ extension RegisteredSubjectsViewModel {
     
     private func loadSubjects() {
         isLoading = false
-        draw()
-        //courseId
-        //subjectName
-        //subjectCode
         
+        
+        registeredSubjects = [.init(courseId: 1, subjectCode: "12", subjectName: "biology", necessary: true),
+                              .init(courseId: 1, subjectCode: "12", subjectName: "biology", necessary: false),
+                              .init(courseId: 1, subjectCode: "12", subjectName: "biology", necessary: true),
+                              .init(courseId: 1, subjectCode: "12", subjectName: "biology", necessary: true),
+                              .init(courseId: 1, subjectCode: "12", subjectName: "biology", necessary: false),
+                              .init(courseId: 1, subjectCode: "12", subjectName: "biology", necessary: true),
+                              .init(courseId: 1, subjectCode: "12", subjectName: "biology", necessary: false)]
+        draw()
+//        isLoading = true
+//        getRegisteredSubjectsUseCase.getSubjects(studentId: SSO.userInfo?.userId?.description ?? "")
+//            .sink { [weak self] completion in
+//                self?.isLoading = false
+//                switch completion {
+//                case .finished:
+//                    self?.draw()
+//                case .failure(_):
+//                    self?.statusBanner = .init(bannerType: .failure,
+//                                               description: "ბოდიშს გიხდით შეფერხებისთის")
+//                }
+//            } receiveValue: { [weak self] model in
+//                self?.registeredSubjects = model
+//            }.store(in: &subscriptions)
     }
 }
 
@@ -70,13 +93,36 @@ extension RegisteredSubjectsViewModel {
     }
     
     func getSubjects() -> [any CellModel]? {
+        guard let registeredSubjects else { return nil }
         var rows: [any CellModel] = []
+        
+        rows.append(getRoundedHeaderWithTitle(title: "დარეგისტრირებული საგნები"))
+        registeredSubjects.enumerated().forEach { index, model in
+            let button: SecondaryButtonModel? = model.necessary
+            ? nil
+            : .init(titleModel: .init(text: "გაუქმება"),
+                    action: { [weak self] in
+                self?.deleteSubject(courseId: model.courseId)
+            })
+            
+            let row = InfoCellModel(topLabelModel: .init(text: model.subjectName,
+                                                         font: .systemFont(ofSize: .XL)),
+                                    bottomLabelModel: .init(text: model.subjectCode,
+                                                            font: .systemFont(ofSize: .L,
+                                                                              weight: .light)),
+                                    buttonModel: button,
+                                    isSeparatorNeeded: index != (registeredSubjects.count - 1))
+            
+            rows.append(row)
+        }
+        rows.append(getRoundedFooterModel())
         
         return rows
     }
     
-    private func deleteSubject() {
-        
+    private func deleteSubject(courseId: Int64) {
+       
+
     }
 }
 
